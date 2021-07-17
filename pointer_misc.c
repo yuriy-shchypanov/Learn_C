93871a022f35c413f5a67b1e378b2019331eac87
Yuriy Shchypanov <yuriy.shchypanov@gmail.com>
2021-07-17 23:55:33 +0300

    ===

---
+++ pointer_misc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    struct person {
+        char pname[6];
+        int age;
+    };
+
+    struct person p = {"Wuriy", 58};
+    char *ch;
+    
+    struct person *ptr_person;
+    ptr_person = &p;
+    ch =(char *) ptr_person;
+    
+    printf("%c",*ch);
+    return 0;
+}
