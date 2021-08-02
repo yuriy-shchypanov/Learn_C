19aac79d60e1df9880708fd8e58455a12eeef9d1
Yuriy Shchypanov <yuriy.shchypanov@gmail.com>
2021-08-02 21:03:05 +0300

    ----

--- strcatNT.c
+++ strcatNT.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #define eos '\0'
 
+// возвращает длину строки
 int length(const char *p_ch)
 {
     int v = 0;
-    for (; *p_ch != '\0'; p_ch++)
-    {
+    for (; *p_ch != eos; p_ch++)
         v += sizeof(*p_ch);
-        //printf("%d=%c ", v, *p_ch);
-    }
     return v;
 }
 
@@ -18,31 +16,89 @@ char *strcatNT(char *dest, const char *source)
     char *retval;
     int v = length(dest) + sizeof(char); // + length(source) + sizeof(char);
 
-    retval = malloc(v);
-    /* for (int i = 0; i<v; i++)
-        retval[i] = dest[i]; */
-    while (*(dest++) != '\0')
+    retval = (char *)malloc(v);
+    int i;
+    for (i = 0; *dest != '\0'; dest++)
     {
-        //*retval = *dest;
-        //dest += 1;
-        printf("%c", *dest);
-        //dest++;
-        //retval += 1;
+        retval[i] = *dest;
+        i++;
+        //++retval;
     }
-    retval += 1;
-    *retval = '\0';
+    //printf("sizeofarray=%d\n",sizeof(retval));
+    retval[i] = '\0';
+
+    return retval;
+}
+
+char *strcatNTP(char *dest, const char *source)
+{
+    char *retval;
+    char *retval_save;
+    int v = length(dest) + length(source) + sizeof(char);
+
+    retval = (char *)malloc(v);
+    retval_save = retval;
+    int i;
+    for (i = 0; *dest != '\0'; dest++)
+    {
+        retval[i] = *dest;
+        i++;
+    }
+
+    for (; *source != '\0'; source++)
+    {
+        retval[i] = *source;
+        i++;
+    }
+    retval[i] = '\0';
+    //retval = retval_save;
+    return retval;
+}
+
+char *strconcat(char *dest, const char *source)
+{
+    char *inner_copy(char *pdest, const char *psource)
+    {
+        char *pstr;
+        pstr = pdest;
+         for (; *psource != '\0'; psource++, pstr++)
+        {
+            *pstr = *psource;
+            //pstr++;
+        }
+        return pstr;
+    }
+    char *retval = NULL;
+    char *retval_start;
+    int v = length(dest) + length(source);
+    if (v > 0)
+    {
+        retval = (char *)malloc(v + sizeof(char));
+        if (!(retval))
+            printf("Error memory allocation");
+        else
+        {
+            retval_start = retval;
+            retval = inner_copy(retval,dest);
+            retval = inner_copy(retval, source);
+            *retval = '\0';
+            retval = retval_start;
+        }
+    }
+    else retval = "";
+
     return retval;
 }
 
 int main(void)
 {
-    char *source = "Yuriy";
-    char *dest = "Shchypanov";
+    char *source = "";
+    char *dest = "";
     char *res;
-
-    //res = strcatNT(dest,source);
-    //printf("%s\n", res);
-    int x = length(dest);
-    printf("\nx=%d", x);
+    //res = strcatNTP(source, dest);
+    res = strconcat(source,dest);
+    printf("=%s", res);
+    printf("\nLength=%d",length(res));
+    free(res);
     return 0;
 }
---
+++ strconcat.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define eos '\0'
+#define emptystr ""
+
+// возвращает длину строки
+int length(const char *p_ch)
+{
+    int v = 0;
+    for (; *p_ch != eos; p_ch++)
+        v += sizeof(*p_ch);
+    return v;
+}
+
+char *strconcat(char *dest, const char *source)
+{
+    char *inner_copy(char *pdest, const char *psource)
+    {
+        char *pstr;
+        pstr = pdest;
+        for (; *psource != eos; psource++, pstr++)
+            *pstr = *psource;
+        return pstr;
+    }
+
+    char *retval = NULL;
+    char *retval_start;
+    int v = length(dest) + length(source);
+    if (v > 0)
+    {
+        retval = (char *)malloc(v + sizeof(char));
+        if (!(retval))
+            printf("Error memory allocation");
+        else
+        {
+            retval_start = retval;
+            retval = inner_copy(retval, dest);
+            retval = inner_copy(retval, source);
+            *retval = eos;
+            retval = retval_start;
+        }
+    }
+    else
+        retval = emptystr;
+
+    return retval;
+}
+
+int main(void)
+{
+    char *source = "Yuriy ";
+    char *dest = "Shchypanov";
+    char *res;
+    res = strconcat(source, dest);
+    printf("=%s", res);
+    printf("\nLength=%d", length(res));
+    free(res);
+    return 0;
+}
